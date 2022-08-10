8abfa15b65004c1aff1a4ba172bbaddb42f2d8de
akshay636 <akshaykhajuriya688@gmail.com>
2022-08-10 15:06:57 +0530

    qs

---
+++ src/components/admin/AddedQues.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/system'
+import React from 'react'
+
+const AddedQues = () => {
+  return (
+   <Container>
+    Contest contain following
+   </Container>
+  )
+}
+
+export default AddedQues
