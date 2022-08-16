1c5362ae779b34b942c116d0eb0abe7ceb0ee993
akshay636 <akshaykhajuriya688@gmail.com>
2022-08-16 11:37:32 +0530

    l

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+// import "./App.css";
+// import AnswerSheet from "./components/admin/AnswerSheet";
+// import Dashbord from "./components/admin/Dashbord";
+// import Login from "./components/auth/Login";
+// import RegisterOne from "./components/auth/RegisterOne";
+// import RegisterTwo from "./components/auth/RegisterTwo";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// function App() {
+//   return (
+//     <>
+//       {/* <Dashbord/>
+//       <Login/>
+//      <RegisterOne/>
+//      <RegisterTwo/>
+//       <AnswerSheet /> */}
+
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Login />}></Route>
+//           <Route path="/register" element={<RegisterOne />}></Route>
+//           <Route path="/password" element={<RegisterTwo/>}></Route>
+//           <Route path="/dashboard" element={<Dashbord/>}></Route>
+//           <Route path="/answersheet" element={<AnswerSheet/>}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
 import "./App.css";
 import AnswerSheet from "./components/admin/AnswerSheet";
 import Dashbord from "./components/admin/Dashbord";
@@ -8,11 +38,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Dashbord/>
+      {/* <Dashbord/>
       <Login/>
      <RegisterOne/>
      <RegisterTwo/>
-      <AnswerSheet />
+      <AnswerSheet /> */}
 
       <BrowserRouter>
         <Routes>
